96bf9e59a804c45768d85df445b056f9d5ed4cd8
techiearunn <arunprasadn@aditi.com>
2013-03-21 19:31:52 +0530

    Added stub for REST service

---
+++ SimulationManager/SimulationManager.Service/Contracts/Experiment.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SimulationManager.Service
+{
+    [DataContract]
+    public class Experiment
+    {
+        public string ProjectId
+        {
+            get;
+            set;
+        }
+    }
+}
